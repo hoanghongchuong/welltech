49b0572d6bf0d3491c0a93c469445e7a3748e55e
hoanghongchuong <chuonghoanghong@gmail.com>
2019-05-12 18:05:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,123 @@
+$(document).ready(function($){	
+	var owl = $(".owl-carousel-product");
+	  	owl.owlCarousel({
+	  		margin:0, 							
+	  		loop:true, 							
+	  		nav:true, 							
+	  		navText:['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>'], 
+	  		autoplay:false, 						
+	  		autoplayTimeout:1500,
+			autoplayHoverPause:true,
+			autoplaySpeed: 2000,
+			responsiveClass:true, 				
+		    responsive:{
+		        0:{
+		            items:1,									            									            
+		        },
+		        600:{
+		            items:2,          
+		        },
+		        1000:{
+		            items:2,  
+		        }
+		    }
+		});
+
+		
+
+		var owl = $(".owl-carousel-slider");
+	  	owl.owlCarousel({
+	  		margin:0, 							
+	  		loop:true, 							
+	  		nav:true, 							
+	  		navText:['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>'], 
+	  		autoplay:false, 						
+	  		autoplayTimeout:1500,
+			autoplayHoverPause:true,
+			autoplaySpeed: 2000,
+			responsiveClass:true, 				
+		    responsive:{
+		        0:{
+		            items:1,									            									            
+		        },
+		        600:{
+		            items:4,          
+		        },
+		        1000:{
+		            items:4,  
+		        }
+		    }
+		});
+
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.slider-nav',
+		 autoplay: true
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  responsive: [{
+ 			breakpoint: 1024,
+ 			settings: {
+ 				slidesToShow:4,
+ 				slidesToScroll: 4
+ 			}
+ 		}, {
+ 			breakpoint: 640,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+			}
+ 		}, {
+ 			breakpoint: 420,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+		}
+ 		}]
+
+	});
+
+
+	// box input search
+	$('.box-search i').on('click', function(){
+		$('.box-search input').addClass('show-input-search');
+	});
+
+});
+
+var MobileMenu = function () {
+        var mobileMenu = $("#menu");
+
+        if (mobileMenu.length) {
+            mobileMenu.mmenu({
+                "extensions": [
+                    // "fx-panels-zoom",
+                    "pagedim-black",
+                    // "theme-dark"
+                ],
+                "offCanvas": {
+                    // "position": "right"
+                },
+                "navbars": [
+                    {
+                        "position": "top",
+                        "content": [
+                            "<a class='fa fa-envelope' href='#/'></a>",
+                            "<a class='fa fa-twitter' href='#/'></a>",
+                            "<a class='fa fa-facebook' href='#/'></a>"
+                        ]
+                    }
+                ]
+            });
+        }
+
+};
